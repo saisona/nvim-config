a159c6e67b06d00c64db1d9fbe1520806ceb1f0a
Alexandre Saison <alexandre.saison@inarix.com>
2024-11-06 18:05:43 +0100

    Update: update deps

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -44,6 +44,22 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      yamlls = {
+        settings = {
+          yaml = {
+            schemas = {
+              kubernetes = "*.yaml",
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+              ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+              ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+              ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+              ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+              ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+            },
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
